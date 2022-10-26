22469801848e2a21007b4a5d7b2126d7a8749945
viktorrn <90602664+viktorrn@users.noreply.github.com>
2022-10-26 17:03:51 +0200

    added audio player

--- DashNBlink/src/game.cpp
+++ DashNBlink/src/game.cpp
@@ -25,7 +25,7 @@ bool game_init()
 
 bool s_running = false;
 
-void game_run()
+void game_run(ma_engine* engine)
 {
     int frames = 0;
     double delta_time = 1000.0 / 90.0;
@@ -71,6 +71,7 @@ void game_run()
         {
             s_running = false;
         }
+ 
     }
 }
 
--- DashNBlink/src/game.h
+++ DashNBlink/src/game.h
@@ -1,7 +1,9 @@
 #pragma once
+#include "miniaudio.h"
+
 
 extern bool game_init();
 
-extern void game_run();
+extern void game_run(ma_engine *engine);
 
 extern void game_clean_up();
